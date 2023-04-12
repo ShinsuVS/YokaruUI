c75483b0374ff463e817ec1590cd1878f821644c
Shugin <Shugin02@mail.ru>
2023-04-12 13:17:53 +0300

    Добавьте файлы проекта.

---
+++ YokaruUI/View/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YokaruUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void dragMe(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch
+            {
+                //trow
+            }
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void rdHome_Click(object sender, RoutedEventArgs e)
+        {
+            // PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("View/Pages/HomePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdSounds_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("View/Pages/SoundsPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdNotes_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("View/Pages/NotesPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void rdPayment_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("View/Pages/PaymentPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void Auth_Click(object sender, RoutedEventArgs e)
+        {
+            PagesNavigation.Navigate(new System.Uri("View/Pages/AuthPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ YokaruUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using YokaruUI.View.Pages;
+
+namespace YokaruUI.ViewModel
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private Page Auth = new AuthPage();
+        private Page Home= new HomePage();
+        private Page Sounds= new SoundsPage();
+        private Page Notes= new NotesPage();
+        private Page _CurPage = new HomePage();
+
+        public Page CurPage
+        {
+            get => _CurPage;
+            set=> Set(ref _CurPage, value);
+        }
+        public ICommand OpenAuthPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Auth);
+            }
+        }
+        public ICommand OpenHomePage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Home);
+            }
+        }
+        public ICommand OpenSoundsPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Sounds);
+            }
+        }
+        public ICommand OpenNotesPage
+        {
+            get
+            {
+                return new RelayCommand( () => CurPage = Notes);
+            }
+        }
+
+    }
+}
